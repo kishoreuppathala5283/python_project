0a8cabfbaca1c07d030aeaf56fc4a7e2cf1a972e
kishoreuppathala5283 <root@kerberos.example.com>
2025-09-24 12:17:01 +0530

    4th commit

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask, request, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html", message="Welcome Devops Team!")
+def hello():
+    return "Hello Team"
 
-@app.route("/greet", methods=["POST"])
-def greet():
-    name = request.form["name"]
-    return f"Hello, {name}!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
