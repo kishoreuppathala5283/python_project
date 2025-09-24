3a0482af1d734279ce050826b9f87a6ad54fea02
kishoreuppathala5283 <root@kerberos.example.com>
2025-09-24 13:41:33 +0530

    10th commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello devops Team99"
+    return "Hello devops Team"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
